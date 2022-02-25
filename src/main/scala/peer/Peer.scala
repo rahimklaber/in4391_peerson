194f633001986f6a9c708bad1977f43f04ae69fa
HallvardMM <hallvardmolinmorstol@gmail.com>
2022-02-25 15:46:47 +0100

    hash logic in peer

--- src/main/scala/peer/Peer.scala
+++ src/main/scala/peer/Peer.scala
@@ -4,7 +4,17 @@ import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 
 
-class Peer(context: ActorContext[PeerMessage]) extends AbstractBehavior[PeerMessage](context) {
+// For hashing: Based on https://stackoverflow.com/questions/46329956/how-to-correctly-generate-sha-256-checksum-for-a-string-in-scala 
+import java.security.MessageDigest
+import java.math.BigInteger
+
+
+
+class Peer(context: ActorContext[PeerMessage], mail: String) extends AbstractBehavior[PeerMessage](context) {
+
+  //Used SHA-256 because MD5 not secure (Check with linux: "echo -n "Test@test.com" | openssl dgst -sha256")
+  val hashedMail = String.format("%064x", new BigInteger(1, MessageDigest.getInstance("SHA-256").digest(mail.getBytes("UTF-8"))))
+  var location = "42.422.171.250" // How should this be solved an IP?
 
 
   override def onMessage(msg: PeerMessage): Behavior[PeerMessage] = {
@@ -16,7 +26,8 @@ class Peer(context: ActorContext[PeerMessage]) extends AbstractBehavior[PeerMess
 }
 
 object Peer{
-  def apply(): Behavior[PeerMessage] = {
-    Behaviors.setup(context => new Peer(context))
+  def apply(mail:String): Behavior[PeerMessage] = {
+
+    Behaviors.setup(context => new Peer(context, mail))
   }
 }

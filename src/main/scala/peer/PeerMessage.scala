f1ec37a2c085a36e929cf2d471eda2fc8b5e97c6
HallvardMM <49036938+HallvardMM@users.noreply.github.com>
2022-03-09 12:58:13 +0100

    Revert "Repl"

--- src/main/scala/peer/PeerMessage.scala
+++ src/main/scala/peer/PeerMessage.scala
@@ -1,33 +1,31 @@
 package peer
 
 import akka.actor.typed.ActorRef
-import dht.File
 
+// Info needed for chat messages
 trait PeerMessage
 
+case class Message(nonHashedSender: String, message : String, ack: Boolean) extends PeerMessage 
+
+case class Example(text: String) extends PeerMessage
+
+case class PeerCmd(cmd: Command) extends PeerMessage // for sending commands to the peer
+
+case class AddWallEntry(text: String) extends PeerMessage
+
+case class FileRequest(fileName: String, version: Int, replyTo : ActorRef[PeerMessage]) extends PeerMessage// for now assume version == 0
+
 /**
- * PeerMessage
- */
-
-case class Message(sender: String, text: String, ack: Boolean) extends PeerMessage
-case class Login(location: String) extends PeerMessage
-
-// for now assume version == 0
-case class FileRequest(fileName: String, version: Int, replyTo: ActorRef[PeerMessage]) extends PeerMessage
-
-/**
+ *
  * @param code response code. Kinda like http response codes.
  */
-case class FileResponse(code: Int, fileName: String, version: Int,
-                        file: Option[File], from: ActorRef[PeerMessage]) extends PeerMessage
-
-/**
- * for sending commands to the peer
- * @param cmd of Command trait
- */
-case class PeerCmd(cmd: Command) extends PeerMessage
+case class FileResponse(code : Int,fileName : String,version : Int, file: Option[File]) extends PeerMessage
 
 
+trait Command
 
+case class GetFile(fileName: String, replyTo : ActorRef[PeerMessage]) extends Command
 
+case class AddToWall(user: String,text: String) extends Command
 
+case class Login(location: String) extends PeerMessage
--- src/main/scala/userData/LocatorInfo.scala
+++ src/main/scala/userData/LocatorInfo.scala
@@ -3,16 +3,20 @@ package userData
 import userData.State.State
 
 case class LocatorInfo(
-                        locator: String, // a string defined by user, to tell on which machine the user is currently active
-                        IP: String,
-                        port: String,
-                        state: State, // only one peerID can be active at a time
-                        // meshID: String, // optional, if a user is participating in a user mesh project
-                        // GPS: String, // optional, coordinate based P2p neighbour selection
-                        // timestamp: Int // time in seconds since 01.01.1970, used fot OpenDHT problems -> maybe not needed
+                        val locator: String, // a string defined by user, to tell on which machine the user is currently active
+                        val IP: String,
+                        val port: String,
+                        val State: State, // only one peerID can be active at a time
+                        // val meshID: String, // optional, if a user is participating in a user mesh project
+                        // val GPS: String, // optional, coordinate based P2p neighbour selection
+                        // val timestamp: Int // time in seconds since 01.01.1970, used fot OpenDHT problems -> maybe not needed
                       )
 
-object State extends Enumeration {
+object State extends Enumeration
+{
   type State = Value
-  val online, active, offline = Value
+
+  val online = Value("online")
+  val active = Value("active")
+  val offline = Value("offline")
 }
